129fe33078feeb1b03bce6bbba80521dd214680f
huahua <869016514@qq.com>
2021-06-01 15:27:10 +0800

    测试提交

--- src/main/java/com/huahua/blog/sys/web/sysController.java
+++ src/main/java/com/huahua/blog/sys/web/sysController.java
@@ -1,8 +1,12 @@
 package com.huahua.blog.sys.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class sysController {
-
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
 }
